c10b37958ad33b31b3f72e76100c80c373f0ea2e
tewalds <tewalds@google.com>
2017-11-07 21:59:13 +0000

    Read the executable location from the ExecuteInfo.txt file for easier use of non-standard installations.
    PiperOrigin-RevId: 174907991

--- pysc2/run_configs/platforms.py
+++ pysc2/run_configs/platforms.py
@@ -50,8 +50,22 @@ def get_version(game_version):
   return VERSIONS[game_version]
 
 
+def read_execute_info(path, parents):
+  """Read the ExecuteInfo.txt file and return the base directory."""
+  path = os.path.join(path, "StarCraft II/ExecuteInfo.txt")
+  if os.path.exists(path):
+    with open(path, "rb") as f:  # Binary because the game appends a '\0' :(.
+      for line in f:
+        parts = [p.strip() for p in line.split("=")]
+        if len(parts) == 2 and parts[0] == "executable":
+          exec_path = parts[1].replace("\\", "/")  # For windows compatibility.
+          for _ in range(parents):
+            exec_path = os.path.dirname(exec_path)
+          return exec_path
+
+
 class LocalBase(lib.RunConfig):
-  """Base run config for the deepmind file hierarchy."""
+  """Base run config for public installs."""
 
   def __init__(self, base_dir, exec_name, cwd=None, env=None):
     base_dir = os.path.expanduser(base_dir)
@@ -92,9 +106,10 @@ class Windows(LocalBase):
   """Run on Windows."""
 
   def __init__(self):
-    super(Windows, self).__init__(
-        os.environ.get("SC2PATH", "C:/Program Files (x86)/StarCraft II"),
-        "SC2_x64.exe", "Support64")
+    exec_path = (os.environ.get("SC2PATH") or
+                 read_execute_info(os.path.expanduser("~/Documents"), 3) or
+                 "C:/Program Files (x86)/StarCraft II")
+    super(Windows, self).__init__(exec_path, "SC2_x64.exe", "Support64")
 
   @classmethod
   def priority(cls):
@@ -106,9 +121,11 @@ class MacOS(LocalBase):
   """Run on MacOS."""
 
   def __init__(self):
-    super(MacOS, self).__init__(
-        os.environ.get("SC2PATH", "/Applications/StarCraft II"),
-        "SC2.app/Contents/MacOS/SC2")
+    exec_path = (os.environ.get("SC2PATH") or
+                 read_execute_info(os.path.expanduser(
+                     "~/Library/Application Support/Blizzard"), 6) or
+                 "/Applications/StarCraft II")
+    super(MacOS, self).__init__(exec_path, "SC2.app/Contents/MacOS/SC2")
 
   @classmethod
   def priority(cls):
