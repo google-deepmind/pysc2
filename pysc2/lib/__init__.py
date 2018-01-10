200dd311f98ae8e2154b464f35e01ff3d136cbdd
tewalds <tewalds@google.com>
2018-01-10 16:39:26 +0000

    Don't export the variables directly on the lib. Instead import the libs directly. This lead to some unexpected dependency problems.
    PiperOrigin-RevId: 181469711

--- pysc2/lib/__init__.py
+++ pysc2/lib/__init__.py
@@ -11,19 +11,3 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Export just the bits of lib that an agent is likely to need.
-
-See ../README.md for more information.
-"""
-
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
-from pysc2.lib.actions import Arguments
-from pysc2.lib.actions import FunctionCall
-from pysc2.lib.actions import FUNCTIONS
-from pysc2.lib.actions import TYPES
-from pysc2.lib.features import FeatureType
-from pysc2.lib.features import MINIMAP_FEATURES
-from pysc2.lib.features import SCREEN_FEATURES
