Fix feature_units observation when you've got no visible units.
PiperOrigin-RevId: 189170978

pysc2/lib/features.py
@@ -692,7 +692,7 @@ class Features(object):
           if u.is_on_screen and u.display_type != sc_raw.Hidden:
             feature_units.append(feature_unit_vec(u))
         out["feature_units"] = named_array.NamedNumpyArray(
-            np.stack(feature_units), [None, FeatureUnit])
+            feature_units, [None, FeatureUnit], dtype=np.int32)
 
     out["available_actions"] = np.array(self.available_actions(obs.observation),
                                         dtype=np.int32)

pysc2/lib/named_array.py
@@ -65,12 +65,16 @@ class NamedNumpyArray(np.ndarray):
     obj = np.array(values, *args, **kwargs)
 
     if len(obj.shape) == 1:
-      # Allow just a single dimension if the array is also single dimension.
-      try:
-        if len(names) > 1:
+      if obj.shape[0] == 0 and names and names[0] is None:
+        # Support arrays of length 0.
+        names = [None]
+      else:
+        # Allow just a single dimension if the array is also single dimension.
+        try:
+          if len(names) > 1:
+            names = [names]
+        except TypeError:  # len of a namedtuple is a TypeError
           names = [names]
-      except TypeError:  # len of a namedtuple is a TypeError
-        names = [names]
 
     # Validate names!
     if not isinstance(names, (list, tuple)) or len(names) != len(obj.shape):
@@ -78,7 +82,7 @@ class NamedNumpyArray(np.ndarray):
           "Names must be a list of length equal to the array shape: %s != %s." %
           (len(names), len(obj.shape)))
     index_names = []
-    only_none = True
+    only_none = obj.shape[0] > 0
     for i, o in enumerate(names):
       if o is None:
         index_names.append(o)