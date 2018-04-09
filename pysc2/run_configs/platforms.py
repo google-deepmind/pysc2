ad4435e8f7b6f0a731accdf95047ebd66cdfceda
Wayne Tsui <tsuiwwwayne@hotmail.com>
2018-04-09 13:09:56 +0100

    Fixed byte to string comparison TypeError in platform run configuration for pysc2.
    PiperOrigin-RevId: 192112739

--- pysc2/run_configs/platforms.py
+++ pysc2/run_configs/platforms.py
@@ -66,7 +66,7 @@ def _read_execute_info(path, parents):
   if os.path.exists(path):
     with open(path, "rb") as f:  # Binary because the game appends a '\0' :(.
       for line in f:
-        parts = [p.strip() for p in line.split("=")]
+        parts = [p.strip() for p in line.decode("utf-8").split("=")]
         if len(parts) == 2 and parts[0] == "executable":
           exec_path = parts[1].replace("\\", "/")  # For windows compatibility.
           for _ in range(parents):
