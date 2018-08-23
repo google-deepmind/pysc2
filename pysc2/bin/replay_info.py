569a5242a75566667735c6cd335be7b1afcff6f9
tewalds <tewalds@google.com>
2018-08-23 16:12:44 +0100

    Give more information about replays that failed.
    PiperOrigin-RevId: 209937716

--- pysc2/bin/replay_info.py
+++ pysc2/bin/replay_info.py
@@ -24,6 +24,7 @@ from absl import app
 from future.builtins import str  # pylint: disable=redefined-builtin
 
 from pysc2 import run_configs
+from pysc2.lib import remote_controller
 
 from pysc2.lib import gfile
 from s2clientprotocol import common_pb2 as sc_common
@@ -51,33 +52,43 @@ def _replay_index(replay_dir):
         "P2-apm",
     )))
 
-    bad_replays = []
-    for file_path in run_config.replay_paths(replay_dir):
-      file_name = os.path.basename(file_path)
-      info = controller.replay_info(run_config.replay_data(file_path))
-      if info.HasField("error"):
-        print("failed:", file_name, info.error, info.error_details)
-        bad_replays.append(file_name)
-      else:
-        out = [
-            file_name,
-            info.base_build,
-            info.map_name,
-            info.game_duration_loops,
-            len(info.player_info),
-            sc_pb.Result.Name(info.player_info[0].player_result.result),
-            sc_common.Race.Name(info.player_info[0].player_info.race_actual),
-            info.player_info[0].player_apm,
-        ]
-        if len(info.player_info) >= 2:
-          out += [
-              sc_common.Race.Name(info.player_info[1].player_info.race_actual),
-              info.player_info[1].player_apm,
+    try:
+      bad_replays = []
+      for file_path in run_config.replay_paths(replay_dir):
+        file_name = os.path.basename(file_path)
+        try:
+          info = controller.replay_info(run_config.replay_data(file_path))
+        except remote_controller.RequestError as e:
+          bad_replays.append("%s: %s" % (file_name, e))
+          continue
+        if info.HasField("error"):
+          print("failed:", file_name, info.error, info.error_details)
+          bad_replays.append(file_name)
+        else:
+          out = [
+              file_name,
+              info.base_build,
+              info.map_name,
+              info.game_duration_loops,
+              len(info.player_info),
+              sc_pb.Result.Name(info.player_info[0].player_result.result),
+              sc_common.Race.Name(info.player_info[0].player_info.race_actual),
+              info.player_info[0].player_apm,
           ]
-        print(u",".join(str(s) for s in out))
-    if bad_replays:
-      print("Replays with errors:")
-      print("\n".join(bad_replays))
+          if len(info.player_info) >= 2:
+            out += [
+                sc_common.Race.Name(
+                    info.player_info[1].player_info.race_actual),
+                info.player_info[1].player_apm,
+            ]
+          print(u",".join(str(s) for s in out))
+    except KeyboardInterrupt:
+      pass
+    finally:
+      if bad_replays:
+        print("\n")
+        print("Replays with errors:")
+        print("\n".join(bad_replays))
 
 
 def _replay_info(replay_path):
