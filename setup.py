c35707aa83a40fb15c4d135dbbb2bd36ec13cfc1
tewalds <tewalds@google.com>
2019-03-05 17:29:08 +0000

    PySC2 supports newer versions of python.
    PiperOrigin-RevId: 236862456

--- setup.py
+++ setup.py
@@ -91,6 +91,9 @@ setup(
         'Operating System :: MacOS :: MacOS X',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Scientific/Engineering :: Artificial Intelligence',
     ],
 )
