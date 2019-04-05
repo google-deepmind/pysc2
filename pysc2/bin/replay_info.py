be37accc248b0cf02eb9eb5d57643e08c94ad99c
tewalds <tewalds@google.com>
2019-04-05 18:07:50 +0100

    Launch the right version of the game to view these replays.
    PiperOrigin-RevId: 242144299

--- pysc2/bin/replay_info.py
+++ pysc2/bin/replay_info.py
@@ -21,27 +21,41 @@ from __future__ import print_function
 import os
 
 from absl import app
+from absl import flags
 from future.builtins import str  # pylint: disable=redefined-builtin
 
 from pysc2 import run_configs
 from pysc2.lib import remote_controller
+from pysc2.lib import replay
 
 from pysc2.lib import gfile
 from s2clientprotocol import common_pb2 as sc_common
 from s2clientprotocol import sc2api_pb2 as sc_pb
+
+FLAGS = flags.FLAGS
 
 
 def _replay_index(replay_dir):
   """Output information for a directory of replays."""
   run_config = run_configs.get()
   replay_dir = run_config.abs_replay_path(replay_dir)
-  print("Checking: ", replay_dir)
+  print("Checking:", replay_dir)
+  replay_paths = list(run_config.replay_paths(replay_dir))
+  print("Found %s replays" % len(replay_paths))
 
+  if not replay_paths:
+    return
+
+  data = run_config.replay_data(replay_paths[0])
+  ver = replay.get_replay_version(data)
+  FLAGS.set_default("sc2_version", ver.game_version)
+  run_config = run_configs.get()  # In case the version changed.
+  print("Launching version:", run_config.version.game_version)
   with run_config.start(want_rgb=False) as controller:
     print("-" * 60)
     print(",".join((
         "filename",
-        "build",
+        "version",
         "map_name",
         "game_duration_loops",
         "players",
@@ -54,10 +68,11 @@ def _replay_index(replay_dir):
 
     try:
       bad_replays = []
-      for file_path in run_config.replay_paths(replay_dir):
+      for file_path in replay_paths:
         file_name = os.path.basename(file_path)
+        data = run_config.replay_data(file_path)
         try:
-          info = controller.replay_info(run_config.replay_data(file_path))
+          info = controller.replay_info(data)
         except remote_controller.RequestError as e:
           bad_replays.append("%s: %s" % (file_name, e))
           continue
@@ -67,7 +82,7 @@ def _replay_index(replay_dir):
         else:
           out = [
               file_name,
-              info.base_build,
+              info.game_version,
               info.map_name,
               info.game_duration_loops,
               len(info.player_info),
@@ -98,8 +113,13 @@ def _replay_info(replay_path):
     return
 
   run_config = run_configs.get()
+  data = run_config.replay_data(replay_path)
+  ver = replay.get_replay_version(data)
+  FLAGS.set_default("sc2_version", ver.game_version)
+  run_config = run_configs.get()  # In case the version changed.
+  print("Launching version:", run_config.version.game_version)
   with run_config.start(want_rgb=False) as controller:
-    info = controller.replay_info(run_config.replay_data(replay_path))
+    info = controller.replay_info(data)
   print("-" * 60)
   print(info)
 
