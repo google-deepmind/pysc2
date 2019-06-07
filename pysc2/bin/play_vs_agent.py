28d19a578d56493358b513b10f9c83169b4ffa93
tewalds <tewalds@google.com>
2019-06-07 15:04:50 +0100

    Add all the recent render options to play_vs_agent for easier testing.
    PiperOrigin-RevId: 252042889

--- pysc2/bin/play_vs_agent.py
+++ pysc2/bin/play_vs_agent.py
@@ -133,6 +133,16 @@ def agent():
           rgb_screen=FLAGS.rgb_screen_size,
           rgb_minimap=FLAGS.rgb_minimap_size,
           action_space=FLAGS.action_space,
+          use_unit_counts=True,
+          use_camera_position=True,
+          show_cloaked=True,
+          show_burrowed_shadows=True,
+          show_placeholders=True,
+          send_observation_proto=True,
+          crop_to_playable_area=True,
+          raw_crop_to_playable_area=True,
+          allow_cheating_layers=True,
+          add_cargo_to_units=True,
           use_feature_units=FLAGS.use_feature_units),
       visualize=FLAGS.render) as env:
     agents = [agent_cls()]
@@ -226,6 +236,11 @@ def human():
     if FLAGS.render:
       join.options.raw = True
       join.options.score = True
+      join.options.raw_affects_selection = True
+      join.options.raw_crop_to_playable_area = True
+      join.options.show_cloaked = True
+      join.options.show_burrowed_shadows = True
+      join.options.show_placeholders = True
       if FLAGS.feature_screen_size and FLAGS.feature_minimap_size:
         fl = join.options.feature_layer
         fl.width = 24
