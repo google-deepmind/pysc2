88437e9fac7e77a0dc46947650276249e03266c6
tewalds <tewalds@google.com>
2019-06-13 11:56:08 +0100

    Add some profiling to the compare binaries, and improve the observation diffing.
    PiperOrigin-RevId: 252999960

--- pysc2/bin/compare_binaries.py
+++ pysc2/bin/compare_binaries.py
@@ -18,6 +18,9 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import collections
+import pprint
+import re
 import sys
 
 from absl import app
@@ -25,13 +28,30 @@ from absl import flags
 import deepdiff
 from pysc2 import run_configs
 from pysc2.lib import replay
+from pysc2.lib import stopwatch
 
 from google.protobuf import json_format
 from s2clientprotocol import sc2api_pb2 as sc_pb
 
 
 FLAGS = flags.FLAGS
+
+flags.DEFINE_bool("diff", False, "Whether to diff the observations.")
+flags.DEFINE_bool("truncate", False, "Whether to truncate the diffs.")
+
+flags.DEFINE_integer("step_mul", 8, "Game steps per observation.")
+flags.DEFINE_integer("count", 100000, "How many observations to run.")
 flags.DEFINE_string("replay", None, "Name of a replay to show.")
+
+
+def truncate(obj):
+  if isinstance(obj, str):
+    return obj[:47] + "..." if len(obj) > 50 else obj
+  if isinstance(obj, dict):
+    return {k: truncate(v) for k, v in obj.items()}
+  if isinstance(obj, list):
+    return [truncate(v) for v in obj]
+  return obj
 
 
 def main(argv):
@@ -66,30 +86,80 @@ def main(argv):
   version = replay.get_replay_version(replay_data)
 
   versions = [version._replace(binary=b) for b in version_names]
-  procs = [run_configs.get(version=v).start(want_rgb=False) for v in versions]
+  timers = [stopwatch.StopWatch() for _ in versions]
+
+  procs = []
+  for v, t in zip(versions, timers):
+    with t("launch"):
+      procs.append(run_configs.get(version=v).start(want_rgb=False))
   controllers = [p.controller for p in procs]
 
+  diff_counts = [0] * len(versions)
+  diff_paths = collections.Counter()
+
   try:
-    for controller in controllers:
-      controller.start_replay(start_replay)
+    print("-" * 80)
+    print(controllers[0].replay_info(replay_data))
+    print("-" * 80)
 
-    while True:
-      for controller in controllers:
-        controller.step()
+    for controller, t in zip(controllers, timers):
+      with t("start_replay"):
+        controller.start_replay(start_replay)
 
-      obs = [controller.observe() for controller in controllers]
-      obs_dict = [json_format.MessageToDict(o) for o in obs]
-      for i, o in enumerate(obs_dict):
-        diff = deepdiff.DeepDiff(obs_dict[0], o)
-        if diff:
-          print("%s: %s" % (i, diff))
+    for _ in range(FLAGS.count):
+      for controller, t in zip(controllers, timers):
+        with t("step"):
+          controller.step(FLAGS.step_mul)
+
+      obs = []
+      for controller, t in zip(controllers, timers):
+        with t("observe"):
+          obs.append(controller.observe())
+
+      if FLAGS.diff:
+        obs_dict = [json_format.MessageToDict(o) for o in obs]
+        diffs = {i: deepdiff.DeepDiff(obs_dict[0], o, significant_digits=3)
+                 for i, o in enumerate(obs_dict[1:], 1)}
+        if any(diffs.values()):
+          print((" Diff on step: %s " %
+                 obs[0].observation.game_loop).center(80, "-"))
+          for i, diff in diffs.items():
+            if diff:
+              print(version_names[i])
+              diff_counts[i] += 1
+              if FLAGS.truncate:
+                diff = truncate(diff)
+              pprint.pprint(diff)
+              print()
+              for _, vals in diff.items():
+                for k in vals.keys():
+                  k = re.sub(r"\d+", "i", k)  # Merge array indices.
+                  diff_paths[k] += 1
 
       if obs[0].player_result:
         break
+  except KeyboardInterrupt:
+    pass
   finally:
     for p in procs:
       p.controller.quit()
       p.close()
+
+  if FLAGS.diff:
+    print(" Diff Counts by binary ".center(80, "-"))
+    for v, count in zip(versions, diff_counts):
+      print(" %5d %s" % (count, v.binary))
+    print()
+
+    print(" Diff Counts by observation path ".center(80, "-"))
+    for v, count in diff_paths.most_common(100):
+      print(" %5d %s" % (count, v))
+    print()
+
+  print(" Timings ".center(80, "-"))
+  for v, t in zip(versions, timers):
+    print(v.binary)
+    print(t)
 
 
 if __name__ == "__main__":
